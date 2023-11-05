1f7ea412c11ef32e9b0f221cff530bcb2715692b
ziy00n <jiyun4188@gmail.com>
2023-11-05 22:57:57 +0900

    #5 : 20154_이 구역의 승자는 누구야?!

---
+++ BAEKJOON/20154_이 구역의 승자는 누구야.py
@@ -0,0 +1,17 @@
+# 백준 20154_이 구역의 승자는 누구야?!
+# 문자열
+
+alpha='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers='32123333113133122212112221'
+
+cnt=0
+s=input()
+
+for i in range(len(s)):
+   cnt += int(numbers[alpha.index(s[i])])
+   cnt %= 10
+
+if cnt % 2 == 0: 
+    print("You're the winner?")
+else:
+    print("I'm a winner!")
