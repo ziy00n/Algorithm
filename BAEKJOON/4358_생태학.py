aaed19643a837f6f178baa00edc7969adf822033
ziy00n <jiyun4188@gmail.com>
2023-11-19 23:38:16 +0900

    #4 : 4358_생태학

---
+++ BAEKJOON/4358_생태학.py
@@ -0,0 +1,23 @@
+# 백준 4358_생태학
+# Data Structure
+
+import sys
+input = sys.stdin.readline
+
+total = 0
+dic = dict()
+while 1:
+    word = input().rstrip()
+    if word == '':
+        break
+    total += 1   
+    if word in dic:   # 전에 이미 나왔으면
+        dic[word] += 1
+    else:
+        dic[word] = 1
+sdic = dict(sorted(dic.items()))
+for i in sdic:
+    a = sdic[i]
+    per = (a / total * 100)
+    
+    print("%s %.4f" %(i, per))
