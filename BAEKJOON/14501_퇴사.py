4e09943fcdb7a2cc44528d2dc7adfbb55f9697a1
ziy00n <jiyun4188@gmail.com>
2023-11-19 23:39:08 +0900

    #1 : 14501_퇴사

---
+++ BAEKJOON/14501_퇴사.py
@@ -0,0 +1,19 @@
+# 백준 14501_퇴사
+# DP
+
+import sys
+input = sys.stdin.readline
+
+data = []
+n = int(input())
+for _ in range(n):
+    data.append(list(map(int, input().split())))
+
+d = [0] * (n+1)
+for i in range(1,n+1):
+    t, p = data[i-1][0], data[i-1][1]
+    if i+t-1 <= n:
+        d[i+t-1] = max(d[i+t-1], d[i-1] + p)
+    if d[i] < d[i-1] :
+        d[i] = d[i-1]
+print(max(d))
